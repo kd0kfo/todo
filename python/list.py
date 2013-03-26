318412a2c79480bcbd061c89b6621e4e431481b4
David Coss <David.Coss@stjude.org>
2013-03-26 10:29:30 -0500

    Changed list script to use tkinter. Command line may still be used with the "--cli" flag.

--- python/list.py
+++ python/list.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python
 import json
 from sys import argv
+from getopt import getopt
 
-if len(argv) == 1:
-    print("Usage: list.py <file name>")
+(opts, args) = getopt(argv[1:], "",["cli"])
+
+if len(args) != 1:
+    print("Usage: list.py [--cli] <file name>")
     exit(1)
 
-fn = argv[1]
+use_cli = False
+fn = args[0]
+
+for (opt,optarg) in opts:
+    while opt[0] == '-':
+        opt = opt[1:]
+    if opt == "cli":
+        use_cli = True
 
 todo = json.loads(open(fn,"r").read())
-counter = 1
-for item in todo:
-    print("{0}: {1}".format(counter,item["message"]))
-    counter += 1
+
+if use_cli:
+    counter = 1
+    for item in todo:
+        print("{0}: {1}".format(counter,item["message"]))
+        counter += 1
+else:
+    from Tkinter import *
+    dialog = Tk()
+    dialog.title("TODO")
+    todolist = Listbox(dialog)
+    todolist.pack()
+
+    for item in todo:
+        todolist.insert(END,item["message"])
+    dialog.mainloop()
